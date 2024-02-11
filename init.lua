f3ee4b3a1f50a7206c6832e2b70c3967c29ab78c
Andrew Borem <aborem@dynamixwebdesign.com>
2024-02-10 18:54:59 -0600

    initial

---
+++ init.lua
@@ -0,0 +1,3 @@
+require("core.base")
+require("core.plugins")
+require("core.plugin_config")
---
+++ lua/core/plugins.lua
@@ -0,0 +1,35 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
+end
+
+vim.opt.rtp:prepend(lazypath)
+
+local plugins = {
+	{
+		"ellisonleao/gruvbox.nvim",
+		"nvim-lualine/lualine.nvim",
+		"nvim-tree/nvim-tree.lua",
+		"nvim-tree/nvim-web-devicons",
+		{ 'williamboman/mason.nvim' },
+		{ 'williamboman/mason-lspconfig.nvim' },
+		{
+			'nvim-treesitter/nvim-treesitter',
+			dependencies = {
+				'nvim-treesitter/nvim-treesitter-textobjects',
+			},
+			build = ':TSUpdate',
+		}
+	}
+}
+
+local opts = {}
+
+require("lazy").setup(plugins, opts)
